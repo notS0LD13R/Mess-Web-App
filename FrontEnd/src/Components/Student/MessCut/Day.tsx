287103182a5648c6026c3e00d467a4ff2a532dfa
notS0LD13R <joelsebin@gmail.com>
2023-05-13 19:51:05 +0530

    Added shake animation to disabled days

--- FrontEnd/src/Components/Student/MessCut/Day.tsx
+++ FrontEnd/src/Components/Student/MessCut/Day.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import {useRef} from 'react'
 import './Day.scss'
+import anime from 'animejs'
+import { JsxElement } from 'typescript'
 
 export interface Dayprop {
     disabled:boolean,
@@ -9,12 +11,32 @@ export interface Dayprop {
 
 
 export  function Day(props:Dayprop) {
+    
+    const ref= useRef<HTMLDivElement>(null)
+
+    const animation=anime({
+        targets:ref.current,
+        keyframes:[
+            {rotateZ:45,scale:1.75},
+            {rotateZ:-45,scale:1.75},
+            {rotateZ:0,scale:1},
+        ],
+        duration:100,
+        autoplay:false,
+        loop:1
+    })
+
+    
     return (
     <div className={
         'Day ' +
         (props.cut ? 'cut ' : '') +
         (props.disabled ? 'disabled ' : '') 
-        }>
+        }
+        onClick={()=>{
+            if (props.disabled) animation.play()
+        }}
+        ref={ref}>
         {props.day}
     </div>
   )
